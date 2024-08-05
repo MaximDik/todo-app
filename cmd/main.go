5cae1d0ee79d1254108785dc26a80a48ec29254b
Maxim Dik <maxim22f@yandex.ru>
2024-08-05 09:06:17 +0300

    added handlers, service, repository

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/MaximDik/todo-app"
+	"github.com/MaximDik/todo-app/pkg/handler"
+	"github.com/MaximDik/todo-app/pkg/repository"
+	"github.com/MaximDik/todo-app/pkg/service"
+)
+
+func main() {
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
